767ce68bed1b67a513e99452d7ebcb3be9f5afe7
Ahito987 <martinelena0987@gmail.com>
2025-04-29 19:02:50 +0200

    app.js

--- app.js
+++ app.js
@@ -93,5 +93,10 @@ function generarCalendario() {
     calendario.appendChild(divMes);
   }
 }
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('service-worker.js')
+    .then(() => console.log('Service Worker registrado'))
+    .catch(err => console.error('Error registrando SW:', err));
+}
 
 mostrarTurnos();
