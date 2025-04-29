26a95bfa5a6a52ee146a5d247bd9b52152fe6b07
Ahito987 <martinelena0987@gmail.com>
2025-04-29 19:01:26 +0200

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,34 @@
-self.addEventListener('install', e => {
-  console.log('Service Worker instalado');
+const CACHE_NAME = 'turnosapp-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/app.js',
+  '/manifest.json',
+  '/assets/icon-192.png',
+  '/assets/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener('fetch', e => {
-  console.log('Interceptando fetch:', e.request.url);
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => 
+      Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))
+      )
+    )
+  );
 });
