4d18cbe9cddc5ce54b908e098bf251875b28ba44
Ahito987 <martinelena0987@gmail.com>
2025-05-01 22:19:54 +0200

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', e => {
-  console.log('Service Worker instalado');
-});
-
-self.addEventListener('fetch', e => {
-  console.log('Interceptando fetch:', e.request.url);
-});
